522262ef78533b8cc69a077094fb1acb87bcb19e
COMLAB507 <COMLAB507@DESKTOP-LR57J5O>
2025-04-04 10:33:04 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Garcia_ancheta.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
